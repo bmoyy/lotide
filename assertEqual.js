3e8e481a2b66aa7f586cc14adf3442dc169471b1
Bastien Moy <moy.bastien@gmail.com>
2023-05-01 14:17:12 -0700

    assert function

---
+++ assertEqual.js
@@ -0,0 +1,6 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) return console.log('🛑Assertion failed: ' + actual + " !== " + expected);
+  return console.log("✅Assertion Passed: " + actual + " === " + expected);
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, '1');
