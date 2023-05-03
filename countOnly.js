748f5b6ffd18c06339b9b7cf3819b5632ae19548
Bastien Moy <moy.bastien@gmail.com>
2023-05-03 13:56:44 -0700

    added countOnly function

---
+++ countOnly.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) return console.log(`🛑Assertion failed: ${actual} !== ${expected}`);
+  return console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+};
+
+const countOnly = function(allItems, itemstoCount) {
+  let result = {};
+  for (const item of allItems) {
+    //count for the true values not the false values in the itemsToCount
+    if(itemstoCount[item]===true){
+      //if the result contains Jason already
+      if(result[item]){
+        result[item] = result[item] + 1;
+      } else { //not present in the result object. So we initialize it.
+        result[item] = 1;
+      }
+    }
+  }
+  return result;
+
+}
