b0601626eea40f22014c2200693a2f415b6dd8a6
Bastien Moy <moy.bastien@gmail.com>
2023-05-04 16:00:39 -0700

    added findKey function

---
+++ findKey.js
@@ -0,0 +1,27 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual !== expected) return console.log(`🛑Assertion failed: ${actual} !== ${expected}`);
+//   return console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+// };
+
+function isTruthy(element) {
+  if (element == true) return true;
+  return false;
+}
+
+const findKey = function(object,callback) {
+  for(key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+
+};
+
+// assertEqual(findKey({
+//   "Blue Hill": 1,
+//   "Akaleri":   0,
+//   "noma":      true,
+//   "elBulli":   null,
+//   "Ora":       true,
+//   "Akelarre":  "hi"
+// }, isTruthy), "Blue Hill");
